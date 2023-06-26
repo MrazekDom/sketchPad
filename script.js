8794112b8b15e9623d6f94390558bb69167fb051
Dominik Mrazek <mrazekdominik@centrum.cz>
2023-06-26 12:36:03 +0200

    Add "click and hold" functionality Change function structure (extracted function from for loop)

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
 const container = document.querySelector('.grid-container');
+let isDrawing = false;
 
-function createGrid(numberOfRows, numberOfColumns){
-    let totalCells = numberOfRows*numberOfColumns;
-    for(let i=0;i<totalCells;i++){
+function createGrid(numberOfCells){
+    for(let i=0;i<numberOfCells;i++){
         let cell = document.createElement('div');
         cell.classList.add('cell')
-        cell.addEventListener('mouseover',function(){
-            cell.classList.add('black');
-        });
-        if(totalCells===4096){
-            cell.classList.add('small')   
-        }
-        else if(totalCells===1024){
-            cell.classList.add('medium')
-        }
-        else{
-            cell.classList.add('big')
-        }
-        
-        container.appendChild(cell);
+        HandleCells(cell, numberOfCells);
     }
 }
 
+function cellPaint(cell) {
+    cell.classList.add('black');
+  }
 
 
+function HandleCells(cell, numberOfCells){
+    cell.addEventListener('mousedown', function() {
+        isDrawing = true;
+        cellPaint(cell);
+    });
+    cell.addEventListener('mousemove', function() {
+        if (isDrawing) {
+            cellPaint(cell);
+        }
+    });
+    cell.addEventListener('mouseup', function() {
+        isDrawing = false;
+    });
+
+    if(numberOfCells===4096){
+        cell.classList.add('small')   
+    }
+    else if(numberOfCells===1024){
+        cell.classList.add('medium')
+    }
+    else{
+        cell.classList.add('big')
+    }
+    
+    container.appendChild(cell);
+}
 
 
 // createGrid(16,16)
-// createGrid(32,32)
-  createGrid(64,64)
+createGrid(1024)
+//   createGrid(4096)
+
+
