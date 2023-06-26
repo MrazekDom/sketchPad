80282cd5171bcd68c2f2a8b3da7363695a8c7996
Dominik Mrazek <mrazekdominik@centrum.cz>
2023-06-26 10:43:00 +0200

    Add 3 sizes of the sketch 16x16 , 32x32, 64x64

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const container = document.querySelector('.grid-container');
 
-function createGrid(numberOfCells){
-    for(let i=0;i<numberOfCells;i++){
+function createGrid(numberOfRows, numberOfColumns){
+    let totalCells = numberOfRows*numberOfColumns;
+    for(let i=0;i<totalCells;i++){
         let cell = document.createElement('div',);
-        cell.classList.add('cell')
+        if(totalCells===4096){
+            cell.classList.add('cell-small')   
+        }
+        else if(totalCells===1024){
+            cell.classList.add('cell-medium')
+        }
+        else{
+            cell.classList.add('cell-big')
+        }
         container.appendChild(cell);
     }
 }
 
-createGrid(256)
+// createGrid(16,16)
+// createGrid(32,32)
+
+ createGrid(64,64)
