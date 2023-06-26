d9f4b5afa4c9e688c948e40561e26974362952b8
Dominik Mrazek <mrazekdominik@centrum.cz>
2023-06-26 10:57:08 +0200

    Add event listener to each cell Add mouseover event listener that makes the div black

--- script.js
+++ script.js
@@ -3,21 +3,29 @@ const container = document.querySelector('.grid-container');
 function createGrid(numberOfRows, numberOfColumns){
     let totalCells = numberOfRows*numberOfColumns;
     for(let i=0;i<totalCells;i++){
-        let cell = document.createElement('div',);
+        let cell = document.createElement('div');
+        cell.classList.add('cell')
+        cell.addEventListener('mouseover',function(){
+            cell.classList.add('black');
+        });
         if(totalCells===4096){
-            cell.classList.add('cell-small')   
+            cell.classList.add('small')   
         }
         else if(totalCells===1024){
-            cell.classList.add('cell-medium')
+            cell.classList.add('medium')
         }
         else{
-            cell.classList.add('cell-big')
+            cell.classList.add('big')
         }
+        
         container.appendChild(cell);
     }
 }
 
+
+
+
+
 // createGrid(16,16)
 // createGrid(32,32)
-
- createGrid(64,64)
+  createGrid(64,64)
